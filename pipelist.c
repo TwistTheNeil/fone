3b4877047456a1f7c7502902bfa4c5ff868f82c2
Neil Castelino <neil.fl93@gmail.com>
2018-07-19 05:02:27 +0000

    use rdwr mode for pipes so there is always a writer This prevents the fd having an EOF every time a writer/client exits

--- pipelist.c
+++ pipelist.c
@@ -2,9 +2,7 @@
 
 typedef struct pipelist {
 	char *name_in;
-	char *name_out;
-	int fd_in;
-	int fd_out;
+	int fd;
 	struct pipelist *next;
 } pipelist;
 
@@ -26,7 +24,6 @@ static const char *sendsmspipe = "npipe_sendsmspipe";
 int create_pipes() {
 	int i, j, err, fd = 0;
 	const char **pipes = calloc(N_PIPE_TYPES, sizeof(char*));
-	const char rw[2][3] = { "_r", "_w" };
 	char *pipe;
 
 	if(pipes == NULL) {
@@ -38,67 +35,54 @@ int create_pipes() {
 	pipes[1] = sendsmspipe;
 
 	for(i=0; i<N_PIPE_TYPES; i++) {
-		for(j=0; j<2; j++) {
-			pipe = calloc(strlen(pipes[i])+strlen(rw[j])+1, sizeof(char));
-			if(pipe == NULL) {
-				fprintf(stderr, "[Error] create_pipes: Couldn't allocate memory\n");
+		pipe = calloc(strlen(pipes[i])+1, sizeof(char));
+		if(pipe == NULL) {
+			fprintf(stderr, "[Error] create_pipes: Couldn't allocate memory\n");
+			free(pipes);
+			return 1;
+		}
+
+		strncpy(pipe, pipes[i], strlen(pipes[i]));
+		pipe[strlen(pipes[i])] = 0;
+
+		if(access(pipe, F_OK) == -1) {
+			err = mkfifo(pipe, 0666);
+			if(err != 0) {
+				fprintf(stderr, "[Error] Can't access or create pipe (%s)\n", pipe);
+				free(pipe);
 				free(pipes);
 				return 1;
 			}
-
-			strncpy(pipe, pipes[i], strlen(pipes[i]));
-			strncat(pipe, rw[j], strlen(rw[j]));
-			pipe[strlen(pipes[i])+strlen(rw[j])] = 0;
-
-			if(access(pipe, F_OK) == -1) {
-				err = mkfifo(pipe, 0666);
-				if(err != 0) {
-					fprintf(stderr, "[Error] Can't access or create pipe (%s)\n", pipe);
-					free(pipe);
-					free(pipes);
-					return 1;
-				}
-			}
-
-			// open the pipe and record the fd
-			if(strncmp(rw[j], "_r", 2) == 0) {
-				fd = open(pipe, O_RDONLY | O_NONBLOCK);
-			} else if (strncmp(rw[j], "_w", 2) == 0) {
-				// Will probably be -1 since there is no thread reading from it
-				// It will need to be dealt with when a read has come through
-				fd = open(pipe, O_WRONLY | O_NONBLOCK);
-			}
-
-			if(fd > maxfd) {
-				maxfd = fd;
-			}
-
-			// I will leave out the names for now
-			// TODO: either add in the names or get rid of them from the struct
-			if(j%2 == 0) {
-				pipelist *temp = calloc(1, sizeof(pipelist));
-				if(temp == NULL) {
-					free(pipe);
-					free(pipes);
-					fprintf(stderr, "[Error] create_pipes: Couldn't allocate memory\n");
-					return 1;
-				}
-
-				temp->fd_in = fd;
-
-				if(head == NULL) {
-					head = temp;
-					tail = temp;
-				} else {
-					tail->next = temp;
-					tail = temp;
-				}
-			} else {
-				tail->fd_out = fd;
-			}
-
-			free(pipe);
 		}
+
+		// open the pipe and record the fd
+		fd = open(pipe, O_RDWR | O_NONBLOCK);
+
+		if(fd > maxfd) {
+			maxfd = fd;
+		}
+
+		// I will leave out the names for now
+		// TODO: either add in the names or get rid of them from the struct
+		pipelist *temp = calloc(1, sizeof(pipelist));
+		if(temp == NULL) {
+			free(pipe);
+			free(pipes);
+			fprintf(stderr, "[Error] create_pipes: Couldn't allocate memory\n");
+			return 1;
+		}
+
+		temp->fd = fd;
+
+		if(head == NULL) {
+			head = temp;
+			tail = temp;
+		} else {
+			tail->next = temp;
+			tail = temp;
+		}
+
+		free(pipe);
 	}
 
 	free(pipes);
@@ -112,7 +96,6 @@ void cleanup_pipes() {
 	while(head != NULL) {
 		i = head;
 		free(head->name_in);
-		free(head->name_out);
 		head = head->next;
 		free(i);
 	}
